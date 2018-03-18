0acaada5f816462a4cd308e8f2f6ea8790edcd50
Ki Song <kiwisquash@gmail.com>
2018-03-18 16:07:37 -0700

    feat: modified index.html for jquery; added event listener in designs.js
    Changes made to index.html
    - Added Code to load jQuery Framework from Google CDN
    - Added an id to the submit button
    - Added ` <meta charset="UTF-8"> ` to head.
    
    Changes made to designs.js
    - added an event listener for the submit button
    - added some variables

--- designs.js
+++ designs.js
@@ -2,6 +2,12 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+var inputHeight, inputWeight;
+var submit = $('#submitButton');
+submit.click(function(){
+	inputHeight = $('#inputHeight');
+	console.log(inputHeight);
+});
 
 function makeGrid() {
 
