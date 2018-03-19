d295717ab2f9bef0c478c93ceab25974e5049109
Ki Song <kiwisquash@gmail.com>
2018-03-18 17:50:10 -0700

    feat: hitting submit with input creates grid of desired dimension
    - grid is formed by adding cells to a table element
    - Currently, each cell is filled in with 'hi' as a place holder
    - todo: simplify code so that the loop references the previous element,
    rather than assigning an id to every single row.

--- designs.js
+++ designs.js
@@ -2,15 +2,25 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-var inputHeight, inputWeight;
-var submit = $('#submitButton');
-submit.click(function(){
-	inputHeight = $('#inputHeight');
-	console.log(inputHeight);
+const submit = $('#submitButton');
+const inputWeight = $('#inputWeight');
+const inputHeight = $('#inputHeight');
+const canvas = $('#pixelCanvas');
+
+submit.on("click",function(evt) {
+	evt.preventDefault(); // Prevents the input field from getting reset
+	makeGrid();
 });
 
 function makeGrid() {
+	canvas.empty();
+	let inputHeightVal = inputHeight.val();
+	let inputWeightVal = inputWeight.val();
+	for (var height = 0; height < inputHeightVal; height++){
+		canvas.append('<tr id=\"row'+height+'\"></li>');
+		for (var weight = 0; weight < inputWeightVal; weight++){
+			$('#row'+height).append('<td>hi</td>');
+		};
+	};
+};
 
-// Your code goes here!
-
-}
