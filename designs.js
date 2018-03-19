038945f8d8d462d91147ebaec9020d01cfaf0d2e
Ki Song <kiwisquash@gmail.com>
2018-03-18 23:56:03 -0700

    feat: Pixel coloring / uncoloring works with a fixed color
    - toDo: need to be able to choose the color

--- designs.js
+++ designs.js
@@ -7,6 +7,7 @@ const inputWeight = $('#inputWeight');
 const inputHeight = $('#inputHeight');
 const canvas = $('#pixelCanvas');
 
+
 submit.on("click",function(evt) {
 	evt.preventDefault(); // Prevents the input field from getting reset
 	makeGrid();
@@ -19,8 +20,17 @@ function makeGrid() {
 	for (var height = 0; height < inputHeightVal; height++){
 		canvas.append('<tr class=\"grid-row\"></li>');
 		for (var weight = 0; weight < inputWeightVal; weight++){
-			$('.grid-row').last().append('<td>hi</td>');
+			$('.grid-row').last().append('<td class=\"grid-cell\" bgcolor=\"white\">hi</td>');
 		};
 	};
 };
 
+canvas.on("click",".grid-cell",function(event) {
+	let cellColor = 'blue';
+	let currentCell = $(event.target);
+	if (currentCell.attr('bgcolor')==='white') {
+		currentCell.attr('bgcolor',cellColor);
+	} else {
+		currentCell.attr('bgcolor','white');
+	};	
+});
