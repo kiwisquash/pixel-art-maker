9092366c4696353ceed22d419c5b81987cfd6235
Ki Song <kiwisquash@gmail.com>
2018-03-19 22:13:47 -0700

    style: consistent quotes, added comments, made code more human readable

--- designs.js
+++ designs.js
@@ -1,36 +1,43 @@
-// Select color input
-// Select size input
+// Define a bunch of constants
+const submit = $('#submitButton'); //submit button
+const inputWeight = $('#inputWeight'); // contains value of width
+const inputHeight = $('#inputHeight'); // contains value of height
+const canvas = $('#pixelCanvas'); // container for the grid
+const pixelColor = $('#colorPicker'); // container for user chosen color 
 
-// When size is submitted by the user, call makeGrid()
-const submit = $('#submitButton');
-const inputWeight = $('#inputWeight');
-const inputHeight = $('#inputHeight');
-const canvas = $('#pixelCanvas');
-const pixelColor = $('#colorPicker');
+// Class names for rows and cells
+const rowClass = 'grid-row';
+const cellClass = 'grid-cell';
 
+// Make grid function.
 function makeGrid() {
-	canvas.empty();
+	canvas.empty(); // Empty the grid before drawing a new one.
 	let inputHeightVal = inputHeight.val();
 	let inputWeightVal = inputWeight.val();
-	for (var height = 0; height < inputHeightVal; height++){
-		canvas.append('<tr class=\"grid-row\"></li>');
-		for (var weight = 0; weight < inputWeightVal; weight++){
-			$('.grid-row').last().append('<td class=\"grid-cell\" bgcolor=\"white\"></td>');
+	for (let height = 0; height < inputHeightVal; height++){
+		canvas.append('<tr></tr>'); // add row per height
+		let lastRow = canvas.find('tr').last();
+		lastRow.addClass(rowClass); // give row class
+		for (let weight = 0; weight < inputWeightVal; weight++){
+			lastRow.append('<td></td>'); // add cell to previous row
+			let lastCell = lastRow.children().last();
+			lastCell.addClass(cellClass); // give cell class
+			lastCell.attr('bgcolor','white'); // make cell white
 		};
 	};
 };
-
-submit.on("click",function(evt) {
+// Submit button creates grid.
+submit.on('click',function(evt) {
 	evt.preventDefault(); // Prevents the input field from getting reset
 	makeGrid();
 });
-
-canvas.on("click",".grid-cell",function(event) {
+// Clicking on cell colors the grid.
+canvas.on('click','.grid-cell',function(event) {
 	let cellColor = pixelColor.val();
 	let currentCell = $(event.target);
 	if (currentCell.attr('bgcolor')==='white') {
-		currentCell.attr('bgcolor',cellColor);
+		currentCell.attr('bgcolor',cellColor); // give blank cell a color
 	} else {
-		currentCell.attr('bgcolor','white');
+		currentCell.attr('bgcolor','white'); // turn colored cell blank
 	};	
 });
