d83a8a81b016348a2e3725c9c4d5417d6724d5a2
Ki Song <kiwisquash@gmail.com>
2018-03-19 22:40:08 -0700

    fix: added missing brace - Also made comments more consistent

--- designs.js
+++ designs.js
@@ -9,9 +9,12 @@ const pixelColor = $('#colorPicker'); // container for user chosen color
 const rowClass = 'grid-row';
 const cellClass = 'grid-cell';
 
-// Make grid function.
+// Cell default color
+const defaultColor = 'white';
+
+// Make grid function
 function makeGrid() {
-	canvas.empty(); // Empty the grid before drawing a new one.
+	canvas.empty(); // Empty the grid before drawing a new one
 	let inputHeightVal = inputHeight.val();
 	let inputWeightVal = inputWeight.val();
 	for (let height = 0; height < inputHeightVal; height++){
@@ -22,22 +25,22 @@ function makeGrid() {
 			lastRow.append('<td></td>'); // add cell to previous row
 			let lastCell = lastRow.children().last();
 			lastCell.addClass(cellClass); // give cell class
-			lastCell.attr('bgcolor','white'); // make cell white
+			lastCell.attr('bgcolor', defaultColor); // make cell default color 
 		};
 	};
 };
-// Submit button creates grid.
+// Submit button creates grid
 submit.on('click',function(evt) {
 	evt.preventDefault(); // Prevents the input field from getting reset
 	makeGrid();
 });
-// Clicking on cell colors the grid.
+// Clicking on cell colors the grid
 canvas.on('click','.grid-cell',function(event) {
 	let cellColor = pixelColor.val();
 	let currentCell = $(event.target);
-	if (currentCell.attr('bgcolor')==='white') {
+	if (currentCell.attr('bgcolor')===defaultColor) {
 		currentCell.attr('bgcolor',cellColor); // give blank cell a color
 	} else {
-		currentCell.attr('bgcolor','white'); // turn colored cell blank
+		currentCell.attr('bgcolor',defaultColor); // turn colored cell blank
 	};	
 });
